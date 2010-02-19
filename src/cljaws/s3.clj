19aeeedec8d841285df7da066ceccbadcebd4420
Tobias Lofgren <ugglan@gmail.com>
2010-02-19 15:27:16 +0100

    added put/get objects

--- src/cljaws/s3.clj
+++ src/cljaws/s3.clj
@@ -3,31 +3,66 @@
   (:import 
    (org.jets3t.service.security AWSCredentials)
    (org.jets3t.service.impl.rest.httpclient RestS3Service)
-   (org.jets3t.service.model S3Object)))
+   (org.jets3t.service.model S3Object)
+   (org.jets3t.service.utils ServiceUtils)))
 
 ;;
 ;; Simple Storage Service / S3
 ;;
 
-(declare *s3-service*)
+(declare *s3-service* *s3-bucket*)
 
 (defmacro with-s3 
   [& body]
   `(binding [*s3-service* (RestS3Service. (AWSCredentials. *aws-id* *aws-key*))]
      ~@body))
 
-(defn create-bucket [bucket]
-  (.createBucket *s3-service* bucket))
-
-(defn delete-bucket [bucket]
-  (.deleteBucket *s3-service* bucket))
-
 (defn list-buckets 
   []
   (map (memfn getName) (.listAllBuckets *s3-service*)))
 
+;
 
-(defn list-bucket [bucket]
-  (let [contents (.listObjects *s3-service* (.getBucket *s3-service* bucket))]
+(defmacro with-bucket 
+  [bucket & body]
+  `(binding [*s3-bucket* (.getOrCreateBucket *s3-service* ~bucket)]
+     ~@body))
+
+(
+defn delete-bucket []
+  (.deleteBucket *s3-service* *s3-bucket*))
+
+
+(defn list-bucket []
+  (let [contents (.listObjects *s3-service* *s3-bucket*)]
     (map bean contents)))
 
+
+(defn put-object [key obj]
+  (cond 
+   (string? obj) (.putObject *s3-service* *s3-bucket* (S3Object. key obj)))
+  )
+
+(defn get-object-details [key]
+  (bean (.getObjectDetails *s3-service* *s3-bucket* key)))
+
+
+(defn get-object [key]
+  (.getObject *s3-service* *s3-bucket* key))
+
+(defn get-object-as-stream [key]
+  (.getDataInputStream (get-object key)))
+
+(defn get-object-as-string 
+  "Get object as utf-8 encoded string, probably not a good idea on big objects."
+  [key]
+  (let [baos (java.io.ByteArrayOutputStream.)
+	is (get-object-as-stream key)]
+    (loop [b (.read is)]
+      (if (neg? b)
+	(String. (.toByteArray baos) "UTF-8")
+	(do (.write baos b)
+	    (recur (.read is)))))))
+
+(defn delete-object [key]
+  (.deleteObject *s3-service* *s3-bucket* key))
--- test/cljaws/s3_test.clj
+++ test/cljaws/s3_test.clj
@@ -2,23 +2,39 @@
   (:use (cljaws s3 core core-test helpers) :reload-all)
   (:use [clojure.test]))
 
-(deftest list-buckets-test
+(deftest buckets-test
   (let [bucket-name (make-unique-name "bucket")]
     (with-aws 
       (with-s3 
-	(create-bucket bucket-name)
+	(with-bucket bucket-name
+	  
+	  (let [result (list-buckets)]
+	    (is (seq? result))
+	    (is (pos? (count result))
+		"Should get list of available bucketsimages")
+	    (is (string? (first result))))
 	
-	(let [result (list-buckets)]
-	  (is (seq? result))
-	  (is (pos? (count result))
-	      "Should get list of available bucketsimages")
-	  (is (string? (first result))))
-	
-	(is (while-or-timeout 
-	     false? 5 
-	     (contains-string? (list-buckets) bucket-name)))
+	  (is (while-or-timeout 
+	       false? 5 
+	       (contains-string? (list-buckets) bucket-name)))
 
-	(delete-bucket bucket-name)
+	  (put-object "testing" "Hello World!")
+
+	  (is (while-or-timeout
+	       false? 10
+	       (= "Hello World!" (get-object-as-string "testing"))))
+
+	  (let [content (list-bucket)]
+	    (is (= 1 (count content)))
+	    (is (= "testing" (:key (first content)))))
+
+	  (delete-object "testing")
+
+	  (is (while-or-timeout 
+	       false? 5
+	       (zero? (count (list-bucket)))))
+
+	  (delete-bucket))
 
 	(is (while-or-timeout
 	     false? 5 
