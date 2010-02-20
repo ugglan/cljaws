935c2d95221679544551141eb68e399d646a7976
Tobias Lofgren <ugglan@gmail.com>
2010-02-20 16:16:12 +0100

    Added manipulation of access control lists

--- src/cljaws/s3.clj
+++ src/cljaws/s3.clj
@@ -5,6 +5,7 @@
    (org.jets3t.service.impl.rest.httpclient RestS3Service)
    (org.jets3t.service.model S3Object)
    (org.jets3t.service.utils ServiceUtils)
+   (org.jets3t.service.acl GroupGrantee Permission GrantAndPermission)
    (org.jets3t.service S3ServiceException)))
 
 ;;
@@ -29,8 +30,7 @@
   `(binding [*s3-bucket* (.getOrCreateBucket *s3-service* ~bucket)]
      ~@body))
 
-(
-defn delete-bucket []
+(defn delete-bucket []
   (.deleteBucket *s3-service* *s3-bucket*))
 
 
@@ -38,25 +38,30 @@ defn delete-bucket []
   (let [contents (.listObjects *s3-service* *s3-bucket*)]
     (map bean contents)))
 
+(defn- put-object* 
+  "Update s3-object with acl of parent bucket and then put it."
+  [obj]
+  (.setAcl obj (bucket-acl*))
+  (.putObject *s3-service* *s3-bucket* obj))
 
 (defn put-object [key obj]
+  "Put a key-object pair into the active bucket. Object can be a
+string or a File. The put object will inherit the access control list
+of the bucket."
   (cond 
-   (string? obj) (.putObject *s3-service* *s3-bucket* (S3Object. key obj))
+   (string? obj) (put-object* (S3Object. key obj))
    (= java.io.File 
-      (class obj)) (.putObject *s3-service* *s3-bucket*
-			       (doto (S3Object. obj)
-				 (.setKey key)))))
-
+      (class obj)) (put-object* (doto (S3Object. obj) (.setKey key)))
+   :else (throw (IllegalArgumentException. 
+		 (str "Can't put object of type " (class obj))))))
 
 (defn get-object-details [key]
   (try (bean (.getObjectDetails *s3-service* *s3-bucket* key))
-       (catch S3ServiceException e
-	 nil)))
+       (catch S3ServiceException e nil)))
 
 (defn get-object [key]
   (try (.getObject *s3-service* *s3-bucket* key)
-       (catch S3ServiceException e
-	 nil)))
+       (catch S3ServiceException e nil)))
 
 (defn get-object-as-stream [key]
   (when-let [obj (get-object key)] 
@@ -75,3 +80,69 @@ defn delete-bucket []
 
 (defn delete-object [key]
   (.deleteObject *s3-service* *s3-bucket* key))
+
+
+; access control
+
+
+(defn bucket-acl*
+  "Access control object of this bucket"
+  [] (.getBucketAcl *s3-service* *s3-bucket*))
+
+
+(def #^{:private true} 
+     parse-grantee 
+     {:all-users GroupGrantee/ALL_USERS
+      :authenticated-users GroupGrantee/AUTHENTICATED_USERS
+      :log-delivery GroupGrantee/LOG_DELIVERY})
+
+(def #^{:private true} 
+     parse-permission
+     {:full-control Permission/PERMISSION_FULL_CONTROL
+      :read Permission/PERMISSION_READ
+      :read-acp Permission/PERMISSION_READ_ACP
+      :write Permission/PERMISSION_WRITE
+      :write-acp Permission/PERMISSION_WRITE_ACP 
+      :revoke-all :revoke-all})
+
+(defn make-grant [[grantee permission]]
+  (let [g (parse-grantee grantee) 
+	p (parse-permission permission)]
+    (if (nil? g) (throw (IllegalArgumentException. 
+			 (str "Unknown grantee: " grantee))))
+    (if (nil? p) (throw (IllegalArgumentException. 
+			 (str "Unknown permission: " permission))))
+    (if (keyword? p)
+      {p g} 
+      (GrantAndPermission. g p))))  
+      
+(defn- update-acl [acl grants]
+  (let [parsed (map make-grant grants) 
+	grant-set (into	#{} (filter (comp not map?) parsed))
+	revoke-set (->> (filter map? parsed) 
+			(map :revoke-all)
+			(filter (comp not nil?)) 
+			(into #{}))]
+    (doseq [grantee revoke-set] (.revokeAllPermissions acl grantee))
+    (doto acl (.grantAllPermissions grant-set))))
+
+
+(defn grant 
+  "Grant the permissions in the map grants to the current bucket or a
+key within the bucket. Any :revoke-all permissions will be executed
+before any grants, so {:all-users :read :all-users :revoke-all} is the
+same as {:all-users :revoke-all :all-users :read}
+
+Example: (grant {:all-users :read})
+         (grant \"mykey\" {:authenticated-users :revoke-all}) " 
+  
+  ([grants]
+     (let [acl (bucket-acl*)]
+       (.setAcl *s3-bucket* (update-acl acl grants)))
+     (.putBucketAcl *s3-service* *s3-bucket* ))
+  
+  ([key grants]
+     (when-let [obj (get-object key)]
+       (let [acl (.getObjectAcl *s3-service* *s3-bucket* key)]
+	 (.setAcl obj (update-acl acl grants)))
+       (.putObject *s3-service* *s3-bucket* obj))))
