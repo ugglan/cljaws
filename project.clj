170d67cc5138f876af5cd8ef9820acb8425e5005
Tobias Lofgren <ugglan@gmail.com>
2010-02-23 22:01:03 +0100

    added mime.types for s3

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
-(defproject cljaws "0.1.0-SNAPSHOT"
-  :description "Clojure wrapper for Amazon Web Services" 
+(defproject cljaws "0.2.1-SNAPSHOT"
+  :description "Clojure wrapper for Amazon Web Services"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
 		 [net.java.dev.jets3t/jets3t "0.7.1"]
 		 [com.google.code.typica/typica "1.6"]
 		 ]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+		     [lein-clojars "0.5.0-SNAPSHOT"]]
+
   :native-path "native" ; force forked jvm as a workaround
   )
